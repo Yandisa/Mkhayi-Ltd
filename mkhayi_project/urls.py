003377f9d1e2b2473c99079a5551f1abe6158eeb
Yandisa <gaju.yandisa@gmail.com>
2026-03-14 23:34:23 +0200

    Update urls.py

--- mkhayi_project/urls.py
+++ mkhayi_project/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib.admin import AdminSite
+from django.views.static import serve
 
 admin.site.site_header = 'Mkhayi Ltd. Administration'
 admin.site.site_title = 'Mkhayi Ltd.'
@@ -11,5 +10,6 @@ admin.site.index_title = 'Site Management Panel'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
-  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    # Always serve media files regardless of DEBUG
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
