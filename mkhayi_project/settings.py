71a394c834ad9e00754127d957557176070e9ed9
Yandisa <gaju.yandisa@gmail.com>
2026-03-20 14:57:11 +0200

    Update settings.py

--- mkhayi_project/settings.py
+++ mkhayi_project/settings.py
@@ -60,7 +60,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'core.context_processors.site_settings',
+                'core.context_processors.global_settings',
             ],
         },
     },
